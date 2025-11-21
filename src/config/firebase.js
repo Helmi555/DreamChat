aefe49009db325658e07284fac86d823ed9e08da
Helmi BA <helmiabdelghani00@gmail.com>
2025-11-21 15:54:27 +0100

    refactor: move Firebase API key to env variable

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCbZ028m8lsETQFgOo4g-IYYGz3fkl6NtI",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "dreamchat-5b840.firebaseapp.com",
   projectId: "dreamchat-5b840",
   storageBucket: "dreamchat-5b840.firebasestorage.app",
   messagingSenderId: "737419729548",
-  appId: "1:737419729548:web:977974c37bee2e5c1379a9"
+  appId: "1:737419729548:web:b41a173ae9c1be421379a9"
 };
 
 const app = initializeApp(firebaseConfig);
